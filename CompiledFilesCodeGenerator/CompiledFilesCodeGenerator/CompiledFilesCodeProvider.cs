8378dabf1b3a39d3703bc136356f72e1df134388
Ali NGame <ngame1390@hotmail.com>
2023-08-27 21:55:49 +0330

    - Some little updates.

---
+++ CompiledFilesCodeGenerator/CompiledFilesCodeGenerator/CompiledFilesCodeProvider.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace CompiledFilesCodeGenerator;
+
+[Generator]
+internal class CompiledFilesCodeProvider : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        var SyntaxTrees = context.Compilation.SyntaxTrees;
+        foreach (var STItem in SyntaxTrees)
+        {
+            var fileName = Path.GetFileName(STItem.FilePath);
+            var codeBehind = STItem.GetText(context.CancellationToken);
+            // Here you can access all files and the contents that are present in the compile time. 
+        }
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+//#if DEBUG
+//        if (!Debugger.IsAttached)
+//        {
+//            Debugger.Launch();
+//        }
+//#endif
+    }
+}
---
+++ CompiledFilesCodeGeneratorTestApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CompiledFilesCodeGeneratorTestApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
